506c68eefd674a84486b6c5506b581b5fd7a6bb4
khushi <khushibanerjee304@gmail.com>
2025-11-17 20:37:51 +0530

    initial commit

---
+++ p1.php
@@ -0,0 +1,8 @@
+<?php 
+echo"1st program";
+function add(int $a, int $b)
+{
+    return $a+$b;
+}
+echo(add(5,"2"))
+?>
---
+++ p2.php
@@ -0,0 +1,9 @@
+<?php
+echo"strict case use- only int data , otherwise throw error";
+declare(strict_types=1);
+function add(int $a, int $b)
+{
+    return $a + $b;
+}
+echo(add(5, "3"));
+?>
